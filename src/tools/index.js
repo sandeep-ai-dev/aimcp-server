17161fff7ec6cf67c0bca2d96d6157a0feb3533a
Sandeep singh <appsdiscover.sandeep@gmail.com>
2025-09-09 14:45:07 +0530

    add mcp

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,6 +1,6 @@
-import { chatGenerateToAiTool, searchAiTool } from "./sendToAi.js";
+import {  searchAiTool } from "./sendToAi.js";
 import { listModelsTool } from "./listModels.js";
 import { healthCheckTool } from "./healthCheck.js";
 import { geminiTool } from "./geminiTool.js"
 import {chatDeleteSessionTool,chatGetHistoryTool,chatGetSessionTool,chatListSessionsTool,chatGenerateTool} from "./chatTools.js"
-export const tools = [chatGenerateToAiTool, searchAiTool, listModelsTool, healthCheckTool,geminiTool,chatDeleteSessionTool,chatGetHistoryTool,chatGetSessionTool,chatListSessionsTool,chatGenerateTool];
+export const tools = [ searchAiTool, listModelsTool, healthCheckTool,geminiTool,chatDeleteSessionTool,chatGetHistoryTool,chatGetSessionTool,chatListSessionsTool,chatGenerateTool];
